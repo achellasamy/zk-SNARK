67ed67b5b5d1233c8c2270910cc602f0aabeafaf
achellasamy <143034266+achellasamy@users.noreply.github.com>
2024-02-09 12:46:35 -0800

    Add files via upload

--- ntt_bram.sv
+++ ntt_bram.sv
@@ -4,66 +4,92 @@ module ntt_bram
         input rst,
         output [11:0] BRAM_addr,
         output BRAM_clk,
-        output [63:0] BRAM_din,
+        output logic [63:0] BRAM_din,
         input [63:0] BRAM_dout,
         output BRAM_en,
         output BRAM_rst,
         output BRAM_we
     );
 
-    localparam BRAM_DEPTH = 4096;
-
     integer i;
 
     logic [63:0] xData [0:63];
     logic [63:0] yData [0:63];
     logic [7:0] wData [0:63][0:63];
-    logic [31:0] address;
-    logic [31:0] counter;
-    logic done;
+    logic [31:0] addressx, addressy;
+    logic [31:0] counterx, countery;
+    logic done, nttrst;
 
-    always_ff (@posedge clk) begin
+    ntt n1 (.x(xData), .w(wData), .clk(clk), .rst(nttrst), .y(yData), .done(done));
+
+    always_ff @(posedge clk) begin
         if(rst) begin
+            counterx <= 0;
             for(i = 0; i < 64; i = i + 1) begin
                 xData[i] = 64'hXXXX_XXXX_XXXX_XXXX;
             end
         end
         else if(!done) begin
-            BRAM_din <= xData[counter];
+            if(counterx < 64) begin
+                xData[counterx] <= BRAM_dout;
+                nttrst <= 1'b1;
+                counterx <= counterx + 1;
+            end
+            else begin
+                nttrst = 1'b0;
+                counterx <= 0;
+            end
         end
     end
 
-    always_ff (@posedge clk) begin
+    always_ff @(posedge clk) begin
         if(rst) begin
+            countery <= 0;
             for(i = 0; i < 64; i = i + 1) begin
                 yData[i] = 64'hXXXX_XXXX_XXXX_XXXX;
             end
         end
         else if(done) begin
-            yData[counter] <= BRAM_dout;
+            if(countery < 64) begin
+                BRAM_din <= yData[countery];
+                nttrst <= 1'b1;
+                counterx <= counterx + 1;
+            end
+            else begin
+                counterx <= 0;
+            end
         end
     end
 
     always_ff @(posedge clk) begin
         if (rst) begin
-            address <= 0;
+            addressx <= 0;
+            addressy <= 64;
         end
         else begin
-            if (seq_valid) begin
-                if (address < BRAM_DEPTH-1) begin
-                    address <= address + 1;
+            if(done) begin
+                if (addressy < 128) begin
+                    addressy <= addressy + 1;
                 end
                 else begin
-                    address <= 0;
+                    addressy <= 64;
                 end            
+            end
+            else begin
+                if (addressx < 64) begin
+                    addressx <= addressx + 1;
+                end
+                else begin
+                    addressx <= 0;
+                end
             end
         end
     end
 
-    assign BRAM_addr = address << 2;
+    assign BRAM_addr = (done) ? addressy << 2 : addressx << 2;
     assign BRAM_clk = clk;
     assign BRAM_en = 1;
     assign BRAM_rst = rst;
-    //assign BRAM_we = 
+    assign BRAM_we = done;
 
 endmodule
